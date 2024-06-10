29198025a8fb1a8353393b0c9904b0fec000b8ff
Tim Fox <tfox@createksolutions.com>
2024-06-10 16:00:24 -0400

    Added chapter 3

---
+++ c3/ex-3-1.R
@@ -0,0 +1,22 @@
+
+# Ex 3-1
+
+
+# a. Construct and store a 4 × 2 matrix that’s filled row-wise with the values 4.3, 3.1, 8.2, 8.2, 3.2, 0.9, 1.6, and 6.5, in that order.
+
+
+# b. Confirm the dimensions of the matrix from (a) are 3 × 2 if you remove any one row.
+
+
+# c. Overwrite the second column of the matrix from (a) with that same column sorted from smallest to largest.
+
+
+# d. What does R return if you delete the fourth row and the first col- umn from (c)? Use matrix to ensure the result is a single-column matrix, rather than a vector.
+
+
+# e. Store the bottom four elements of (c) as a new 2 × 2 matrix.
+
+
+# f. Overwrite, in this order, the elements of (c) at positions (4,2), (1,2), (4,1), and (1,1) with −12 of the two values on the diagonal of (e).
+
+  
