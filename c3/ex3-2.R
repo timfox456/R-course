32824e126498857e5835430aa4b6c0c4980e71b7
Tim Fox <tfox@createksolutions.com>
2024-06-11 08:53:16 -0400

    Added ex3-2

---
+++ c3/ex3-2.R
@@ -0,0 +1,63 @@
+
+# Ex 3-2
+
+
+# a.  Calculate the following
+
+# (2/7) * (M1 - M2)
+
+# M1: 
+# ------
+#      [,1] [,2]
+# [1,]    1    2
+# [2,]    2    4
+# [3,]    7    6
+
+# M2:
+# ------
+#      [,1] [,2]
+# [1,]   10   20
+# [2,]   30   40
+# [3,]   50   60
+
+
+
+# b. Store these two matrices
+
+#     [,1]
+# [1,]    1
+# [2,]    2
+# [3,]    7
+
+
+    # [,1]
+# [1,]    3
+# [2,]    4
+# [3,]    8
+
+# Which of the following multiplications are possible? For those that are, compute the result.
+
+# i. A·B
+
+# ii. A⊤·B
+
+# iii. B⊤·(A·A⊤)
+
+# iv. (A·A⊤)·B⊤
+
+# v. [(B·B⊤)+(A·A⊤)−100I3]−1
+
+
+
+# c. For the following matrix
+
+# A <- rbind(c(2,0,0,0),c(0,3,0,0),c(0,0,5,0),c(0,0,0,-1))
+
+#       [,1] [,2] [,3] [,4]
+# [1,]    2    0    0    0
+# [2,]    0    3    0    0
+# [3,]    0    0    5    0
+# [4,]    0    0    0   -1
+
+# confirm that A−1 · A − I4 provides a 4 × 4 matrix of zeros.
+
