12f34401dc59ac28c2c6520a8b30ca2881342ab9
Tim Fox <tfox@createksolutions.com>
2024-06-14 15:03:31 -0400

    Added solutions

--- c13/ex13-2-solution.R
+++ c13/ex13-2-solution.R
@@ -1,25 +1,61 @@
 ##########
 ## 13.2 ##
 ##########
-#(a)
+# a. Obtain, rounded to two decimal places, the proportion of seis- mic events
+# in the quakes data frame that occurred at a depth of 300 km or deeper.
+
 round(mean(quakes$depth>=300),2)
-#(b)
+
+# b. Remaining with the quakes data set, calculate the mean and median
+# magnitudes of the events that occurred at a depth of 300 km or deeper
+
 mean(quakes$mag[quakes$depth>=300])
 median(quakes$mag[quakes$depth>=300])
-#(c)
+
+# c. Using the chickwts data set, write a for loop that gives you the mean
+# weight of chicks for each feed type—the same as the results given by the
+# tapply function in Section 13.2.1. Display the results rounded to one decimal
+# place and, when printing, ensure each mean is labeled with the appropriate
+# feed type.
+
 for(i in levels(chickwts$feed)){
   cat(i,": ",round(mean(chickwts$weight[chickwts$feed==i]),1)," grams\n",sep="")
 }
+
+# Another ready-to-use data set (in the automatically loaded datasets package)
+# is InsectSprays. It contains data on the number of insects found on various
+# agricultural units, as well as the type of insect spray that was used on each
+# unit. Ensure you can access the data frame at the prompt; then study the help
+# file ?InsectSprays to get an idea of R’s representation of the two variables.
+
+
+
+# d. Identify the two variable types in InsectSprays (as per the defini- tions
+# in Section 13.1.1 and Section 13.1.2).
+
 #(d) count: numeric-discrete; spray: categorical-nominal
-#(e)
+
+# e. Calculate the modes of the distribution of insect counts, regard- less of
+# spray type.
+
 Ctab <- table(InsectSprays$count)
 Ctab[Ctab==max(Ctab)]
-#(f)
+
+# f. Use tapply to report the total insect counts by each spray type.
+
 tapply(InsectSprays$count,INDEX=InsectSprays$spray,FUN=sum)
-#(g)
+
+# g. Using the same kind of for loop as in (c), compute the percent- age of
+# agricultural units in each spray type group that had at least five bugs on
+# them. When printing to the screen, round the percentages to the nearest whole
+# number.
+
 for(i in levels(InsectSprays$spray)){
   cat("Spray ",i,"; at least 5 bugs: ",round(mean(InsectSprays$count[InsectSprays$spray==i]>=5)*100,0),"% \n",sep="")
 }
-#(h)
+
+# h. Obtain the same numeric results as in (g), with rounding, but use tapply
+# and a disposable function.
+
 tapply(InsectSprays$count,INDEX=InsectSprays$spray,FUN=function(x) round(mean(x>=5)*100,0))
 
--- c13/ex13-3-solution.R
+++ c13/ex13-3-solution.R
@@ -1,30 +1,52 @@
-
-######################################
-#### SUGGESTED EXERCISE SOLUTIONS ####
-######################################
-
-
-
 ##########
 ## 13.3 ##
 ##########
-#(a)
+# Using the chickwts data frame, compute the 10th, 30th, and 90th percentiles
+# of all the chick weights and then use tapply to determine which feed type is
+# associated with the highest sample variance of weights.
+
 quantile(chickwts$weight,c(0.1,0.3,0.9))
 chickvars <- tapply(chickwts$weight,INDEX=chickwts$feed,FUN=var)
 chickvars[chickvars==max(chickvars)]
-#(b)
-##(i)
+
+# b. Turn to the seismic event data in quakes and complete the follow- ing tasks:
+
+# i. Find the IQR of the recorded depths.
+
 IQR(quakes$depth)
-##(ii)
+
+# ii. Find the five-number summary of all magnitudes of seismic events that
+# occur at a depth of 400 km or deeper. Compare this to the summary values
+# found in Section 13.2.3 of those events occurring at less than 400 km and
+# briefly comment on what you notice.
+
 summary(quakes$mag[quakes$depth>=400]) ### Magnitudes at <400 have a similar amount of spread as those at >=400 (and an equivalent IQR of 0.5), but are centered at slightly higher values.
+
 ##(iii)
+# iii. Use your knowledge of cut (Section 4.3.3) to create a new factor vector
+# called depthcat that identifies four evenly spaced categories of quakes$depth
+# so that when you use levels(depthcat), it gives the following:
+
+levels(depthcat)
+#    [1] "[40,200)"  "[200,360)" "[360,520)" "[520,680]"
+
 dmin <- min(quakes$depth)
 dmax <- max(quakes$depth)
 depthcat <- cut(quakes$depth,breaks=seq(dmin,dmax,length=5),include.lowest=TRUE,right=FALSE)
 levels(depthcat)
+
+# iv. Find the sample mean and standard deviation of the mag- nitudes of the
+# events associated with each category of depth according to depthcat.
+
 ##(iv)
 tapply(quakes$mag,INDEX=depthcat,FUN=mean)
 tapply(quakes$mag,INDEX=depthcat,FUN=sd)
+
+# Use tapply to compute the 0.8th quantile of the magnitudes of the seismic
+# events in quakes, split by depthcat.
 ##(v)
 tapply(quakes$mag,INDEX=depthcat,FUN=quantile,prob=0.8)
+
+
+
 
