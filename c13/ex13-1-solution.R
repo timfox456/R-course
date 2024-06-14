7bdb7d4472163a2ea3af9399176b8b088fa5f3f7
Tim Fox <tfox@createksolutions.com>
2024-06-14 14:23:44 -0400

    Addd exercise 13

--- c13/ex13-1-solution.R
+++ c13/ex13-1-solution.R
@@ -1,21 +1,59 @@
 
-######################################
-#### SUGGESTED EXERCISE SOLUTIONS ####
-######################################
-
 ##########
 ## 13.1 ##
 ##########
-#(a)
+
+# a. For each of the following, identify the type of variable described:
+# numeric-continuous, numeric-discrete, categorical-nominal, or
+# categorical-ordinal:
+
+# i. The number of blemishes on the hood of a car coming off a production line
+
 ##(i) Numeric-discrete
+
+# ii. A survey question that asks the participant to select from Strongly
+# agree, Agree, Neutral, Disagree, and Strongly disagree
 ##(ii) Categorical-ordinal
+
+
+# iii. The noise level (in decibels) at a concert
 ##(iii) Numeric-continuous
+
+# iv. The noise level out of three possible choices: high, medium, low
 ##(iv) Categorical-ordinal
+
+# v. A choice of primary color
+
 ##(v) Categorical-nominal
+
+# vi. The distance between a cat and a mouse
 ##(vi) Numeric-continuous
+
 #(b)
+
+
+# b. For is a population parameter or a sample statistic. If the latter, also
+# identify what the corresponding population parameter is.
+
+# i. The percentage of 50 New Zealanders who own a gaming console
+
+# ii. The average number of blemishes found on the hoods of three cars in the
+# No Dodgy Carz yard
+
+# iii. The proportion of domestic cats in the United States that wear a collar
+
+# iv. The average number of times per day a vending machine is used in a year
+
+# v. The average number of times per day a vending machine is used in a year,
+# based on data collected on three distinct days in that year
+
 ##(i) Sample statistic. Population parameter is the proportion of NZers who own a gaming console.
 ##(ii) Sample statistic. Population parameter is the average number of blemishes on the hoods of all cars at No Dodgy Carz.
 ##(iii) Population parameter.
 ##(iv) Population parameter.
 ##(v) Sample statistic. Population parameter is (iv).
+
+
+
+
+
--- c13/ex13-3.R
+++ c13/ex13-3.R
@@ -5,14 +5,25 @@
 # associated with the highest sample variance of weights.
 
 
+# Turn to the seismic event data in quakes and complete the follow- ing tasks:
 
-Turn to the seismic event data in quakes and complete the follow- ing tasks:
-i. Find the IQR of the recorded depths.
-ii. Find the five-number summary of all magnitudes of seismic
-events that occur at a depth of 400 km or deeper. Compare this to the summary values found in Section 13.2.3 of those events occurring at less than 400 km and briefly comment on what you notice.
-iii. Use your knowledge of cut (Section 4.3.3) to create a new factor vector called depthcat that identifies four evenly spaced categories of quakes$depth so that when you use levels(depthcat), it gives the following:
-    R> levels(depthcat)
-    [1] "[40,200)"  "[200,360)" "[360,520)" "[520,680]"
-iv. Find the sample mean and standard deviation of the mag- nitudes of the events associated with each category of depth according to depthcat.
-Use tapply to compute the 0.8th quantile of the magnitudes of the seismic events in quakes, split by depthcat.
+# i. Find the IQR of the recorded depths.
+
+# ii. Find the five-number summary of all magnitudes of seismic events that
+# occur at a depth of 400 km or deeper. Compare this to the summary values
+# found in Section 13.2.3 of those events occurring at less than 400 km and
+# briefly comment on what you notice.
+
+# iii. Use your knowledge of cut (Section 4.3.3) to create a new factor vector
+# called depthcat that identifies four evenly spaced categories of quakes$depth
+# so that when you use levels(depthcat), it gives the following:
+
+levels(depthcat)
+#    [1] "[40,200)"  "[200,360)" "[360,520)" "[520,680]"
+
+# iv. Find the sample mean and standard deviation of the mag- nitudes of the
+# events associated with each category of depth according to depthcat.
+
+# Use tapply to compute the 0.8th quantile of the magnitudes of the seismic
+events in quakes, split by depthcat.
 
