9f850577c3e0f78625dae951f1a4ad01fce0f63c
Tim Fox <tfox@createksolutions.com>
2024-06-25 17:24:21 +0300

    Add 18

---
+++ c18/ex18-1.R
@@ -0,0 +1,20 @@
+# Ex 18.1
+
+ # Adult domestic cats of a certain breed are said to have an average weight of
+# 3.5 kilograms. A feline enthusiast disagrees and collects a sample of 73
+# weights of cats of this breed. From her sample, she calculates a mean of 3.97
+# kilograms and a standard deviation of 2.21 kilograms. Perform a hypothesis
+# test to test her claim that the true mean weight μ is not 3.5 kilograms by
+# setting up the appropriate hypothesis, carrying out the analysis, and
+# interpret- ing the p-value (assume the significance level is α = 0.05).
+
+# b. Suppose it was previously believed that the mean magnitude of seismic
+# events off the coast of Fiji is 4.3 on the Richter scale. Use the data in the
+# mag variable of the ready-to-use quakes data set, providing 1,000 sampled
+# seismic events in that area, to test the claim that the true mean magnitude
+# is in fact greater than 4.3. Set up appropriate hypotheses, use t.test
+# (conduct the test at a significance level of α = 0.01), and draw a
+# conclusion.
+
+# c. Manually compute a two-sided confidence interval for the true mean of (b).
+
---
+++ c18/ex18-2.R
@@ -0,0 +1,34 @@
+# #Ex 18-2
+
+# In the package MASS you’ll find the data set anorexia, which contains data on
+# pre- and post-treatment weights (in pounds) of 72 young women suffering from
+# the disease, obtained from Hand et al. (1994). One group of women is the
+# control group (in other words, no inter- vention), and the other two groups
+# are the cognitive behavioral program and family support intervention program
+# groups. Load the library and ensure you can access the data frame and
+# understand its contents. Let μd denote the mean difference in weight,
+# computed as (post-weight − pre-weight).
+
+# a. Regardless of which treatment group the participants fall into, conduct
+# and conclude an appropriate hypothesis test with α = 0.05 for the entire set
+# of weights for the following hypotheses:
+
+# H0 : μd = 0 
+# HA : μd > 0
+
+# b. Next, conduct three separate hypothesis tests using the same defined
+# hypotheses, based on which treatment group the partici- pants fall into. What
+# do you notice?
+
+# Another ready-to-use data set in R is PlantGrowth (Dobson, 1983), which
+# records a continuous measure of the yields of a certain plant, looking at the
+# potential effect of two supplements administered during growth to increase
+# the yield when compared to a control group with no supplement.
+
+# c. Set up hypotheses to test whether the mean yield for the control group is
+# less than the mean yield from a plant given either of the treatments.
+# Determine whether this test should proceed using a pooled estimate of the
+# variance or whether Welch’s t-test would be more appropriate.
+
+# d. Conduct the test and make a conclusion (assuming normality of the raw
+# observations).
