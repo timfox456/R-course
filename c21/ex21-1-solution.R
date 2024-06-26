fdaf892a5dd426baecf192515cdc5656c7ad8d1d
Tim Fox <tfox@createksolutions.com>
2024-06-27 01:00:53 +0800

    Added solution

--- c21/ex21-1-solution.R
+++ c21/ex21-1-solution.R
@@ -1,29 +1,72 @@
 ##########
 ## 21.1 ##
 ##########
-library("MASS")
-?cats
+
+# Ex 21.1
+
+# In the MASS package, you’ll find the data frame cats, which provides data on
+#sex, body weight (in kilograms), and heart weight (in grams) for 144 household
+#cats (see Venables and Ripley, 2002, for further details); you can read the
+#documentation with a call to ?cats. Load the MASS package with a call to
+#library("MASS"), and access the object directly by entering cats at the
+#console prompt.
+
+cats = read.csv("cats.csv")
+
+
+cats <- read.csv("cats.csv"0
+
+
+# a. Plot heart weight on the vertical axis and body weight on the horizontal
+# axis, using different colors or point characters to distinguish between male
+# and female cats. Annotate your plot with a legend and appropriate axis
+# labels.
+
 #(a)
 plot(cats$Bwt,cats$Hwt,col=cats$Sex,xlab="Body weight (kg)",ylab="Heart weight (g)")
 legend("topleft",legend=c("female","male"),col=c(1,2),pch=c(1,1)) # Females are black, since the levels of the factor vector cats$Sex are in the alphabetical order of "F" "M", which R interprets as 1 and 2 when this factor vector is passed to the 'col' argument of 'plot'.
-#(b)
+
+# b. Fit a least-squares multiple linear regression model using heart weight as
+# the response variable and the other two variables as predictors, and view a
+# model summary.
+
 cats.fit <- lm(Hwt~Bwt+Sex,data=cats)
 summary(cats.fit)
+
 ##(i)
+# i. Write down the equation for the fitted model and interpret the estimated
+# regression coefficients for body weight and sex. Are both statistically
+# significant? What does this say about the relationship between the response
+# and predictors?
+
 # "Mean heart weight" = -0.415 + 4.076*"Body weight" - 0.082*"is male"
 # For cats of the same sex, the effect of an additional kg of body weight is, on average, an extra 4.076 grams of heart weight. For cats of the same body weight, the heart weight of a male is, on average, 0.082 grams lighter than that of a female.
 # The model states the effect of body weight is highly statistically significant -- there is evidence to believe body weight does indeed affect heart weight. However, the effect of sex is not significant. There is no statistical evidence to suggest the coefficient for "is male" is any different to zero (when also adjusting for body weight).
 # The above notes imply that the inclusion of "sex" as a predictor is statistically unnecessary when it comes to modeling the response for these data.
-##(ii)
+
+
+# ii. Report and interpret the coefficient of determination and the outcome of
+# the omnibus F -test.
+
 names(summary(cats.fit))
 summary(cats.fit)$r.squared
 # The coefficient of determination, 'R-squared', shows that for your fitted model, 64.5% of the variation in heart weight is able to be captured by the regression.
 summary(cats.fit)$fstatistic
 1-pf(129.1056,df1=2,df2=141)
 # Reading from the summary output, or running the line above, the result of the omnibus F-test is a tiny p-value; effectively zero. This suggests very strong evidence against the null hypothesis (H0 is that modeling heart weight isn't improved by taking body weight and sex into account).
-#(c)
+
+
+# c. Tilman’s cat, Sigma, is a 3.4 kg female. Use your model to esti- mate her
+# mean heart weight and provide a 95 percent prediction interval.
+
 predict(cats.fit,newdata=data.frame(Bwt=3.4,Sex="F"),interval="prediction",level=0.95)
-#(d)
+
+# d. Use predict to superimpose continuous lines based on the fitted linear
+# model on your plot from (a), one for male cats and one for female. What do
+# you notice? Does this reflect the statistical significance (or lack thereof)
+# of the parameter estimates?
+
+
 plot(cats$Bwt,cats$Hwt,col=cats$Sex,xlab="Body weight (kg)",ylab="Heart weight (g)")
 legend("topleft",legend=c("female","male"),col=c(1,2),pch=c(1,1))
 Bwt.seq <- seq(min(cats$Bwt)-0.5,max(cats$Bwt)+0.5,length=30)
@@ -33,17 +76,59 @@ lines(Bwt.seq,cats.pred[1:n],col=2)
 lines(Bwt.seq,cats.pred[(n+1):(2*n)])
 # The two superimposed lines are positively sloped according to the coefficient for "Bwt", but extremely close together, mirroring the minimal impact (and lack of statistical significance) of "Sex".
 #(e)
+
+# The boot package (Davison and Hinkley, 1997; Canty and Ripley, 2015) is
+# another library of R code that’s included with the standard installation but
+# isn’t automatically loaded. Load boot with a call to library("boot"). You’ll
+# find a data frame called nuclear, which contains data on the construction of
+# nuclear power plants in the United States in the late 1960s (Cox and Snell,
+# 1981).
+
+# e. Access the documentation by entering ?nuclear at the prompt and examine
+# the details of the variables. (Note there is a mistake for date, which
+# provides the date that the construction permits were issued—it should read
+# “measured in years since January 1 1900 to the nearest month.”) Use pairs to
+# produce a quick scatterplot matrix of the data.
+
+
 library("boot")
 ?nuclear
 pairs(nuclear)
+
+# f. One of the original objectives was to predict the cost of further
+# construction of these power plants. Create a fit and summary of a linear
+# regression model that aims to model cost by t1 and t2, two variables that
+# describe different elapsed times associated with the application for and
+# issue of various permits. Take note of the estimated regression coefficients
+# and their significance in the fitted model.
 #(f)
+
 nuc.fit1 <- lm(cost~t1+t2,data=nuclear)
 summary(nuc.fit1)
-#(g)
+
+# g. Refit the model, but this time also include an effect for the date the
+# construction permit was issued. Contrast the output for this new model
+# against the previous one. What do you notice, and what does this information
+# suggest about the relationships in the data with respect to these predictors?
+
 nuc.fit2 <- lm(cost~t1+t2+date,data=nuclear)
 summary(nuc.fit2)
-# By including "date" in the linear model, this completely removes the statistical significance of "t1" as seen in the previous model. In fact, the coefficient for "t1" changes signs! What this implies is the previous positive, significant relationship between "t1" and "cost" is actually explained more by "date", owing to the positive correlation of "t1" with "date", and it is "date" that should probably be used in a fitted model. The "t2" predictor remains non-significant, albeit with a reduced p-value in this latest model.
-#(h)
+
+# By including "date" in the linear model, this completely removes the
+  # statistical significance of "t1" as seen in the previous model. In fact,
+  # the coefficient for "t1" changes signs! What this implies is the previous
+  # positive, significant relationship between "t1" and "cost" is actually
+  # explained more by "date", owing to the positive correlation of "t1" with
+  # "date", and it is "date" that should probably be used in a fitted model.
+  # The "t2" predictor remains non-significant, albeit with a reduced p-value
+  # in this latest model.
+
+# h. Fit a third model for power plant cost, using the predictors for “date of
+# permit issue,” “power plant capacity,” and the binary variable describing
+# whether the plant was sited in the north- eastern United States. Write down
+# the fitted model equation and provide 95 percent confidence intervals for
+# each estimated coefficient.
+
 nuc.fit3 <- lm(cost~date+cap+ne,data=nuclear)
 summary(nuc.fit3) # Fitted model is "cost" = -6458 + 95.4*"date of permit issue" + 0.42*"capacity" + 126.1*"constructed in north-east"
 confint(nuc.fit3) # All intervals exclude null value of zero, reflecting their significance in the model summary.
@@ -167,9 +252,40 @@ coef(car.fit)[4] + coef(car.fit)[6] # When the car has 6 cylinders, the slope fo
 coef(car.fit)[4] + coef(car.fit)[7] # When the car has 8 cylinders, the slope for hp is -0.0995 + 0.0860 = -0.0135 (to 4 decimal places)
 # This model suggests that as hp increases, mean MPG decreases (for a fixed wt). However, in comparison to 4-cylinder cars, mean MPG is estimated to decrease at a slower rate with an increasing hp for both 6- and 8-cylinder cars (since the positive additive terms to the baseline slope of -0.0995 still provide negative slopes in hp, but ones that are closer to zero).
 #(h)
-##(i)
+
+# The following table gives an excerpt of a historical data set com- piled
+# between 1961 and 1973. It concerns the annual murder rate in Detroit,
+# Michigan; the data were originally presented and analyzed by Fisher (1976)
+# and are reproduced here from Harraway (1995). In the data set you’ll find the
+# number of murders, police officers, and gun licenses issued per 100,000
+# population, as well as the overall unemployment rate as a percentage of the
+# overall population.
+
+
+# i. Create your own data frame in your R workspace and produce a scatterplot
+# matrix. Which of the variables appears to be most strongly related to the
+# murder rate?
+
 predict(car.fit,newdata=data.frame(wt=c(2.1,3.9,2.9),hp=c(100,210,200),cyl=c(4,8,6)),interval="confidence",level=0.95)
+
 # The first car is the only car that has a point estimate of mean MPG that is higher than your mother's demand of 25, so this would be the initial choice.
+
 ##(ii)
 # Although the point estimate for Car 3 is much less than 25, looking at the confidence intervals you can see that the interval for Car 3 includes 25. So, you could argue to your mother that you're 95% confident that the true mean MPG of a car like Car 3 lies somewhere in that interval; in particular, your model suggests no evidence against the hypothesis that the true mean MPG of such a car is equal to 25. Of course, the interval also includes possible true values that are far worse than 25... but you don't need to tell your mother that.
+
+# j. Fit a multiple linear regression model using the number of murders as the
+# response and all other variables as predictors. Write down the model equation
+# and interpret the coefficients. Is it reasonable to state that all
+# relationships between the response and the predictors are causal?
+
+# k. Identify the amount of variation in the response attributed to the joint
+# effect of the three explanatory variables. Then refit the model excluding the
+# predictor associated with the largest (in other words, “most nonsignificant”)
+# p-value. Compare the new coefficient of determination with that of the
+# previous model. Is there much difference?
+
+# l. Use your model from (k) to predict the mean number of mur- ders per
+# 100,000 residents, with 300 police officers and 500 issued gun licenses.
+# Compare this to the mean response if there were no gun licenses issued and
+# provide 99 percent confidence intervals for both predictions.
 
